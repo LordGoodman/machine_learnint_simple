1b0fbc4dc616d8a32744ac788ec13d6a2e58a705
LordGoodman <201negative@sina.com>
2015-11-14 01:11:48 +0800

    still constructing

--- knn.py
+++ knn.py
@@ -10,15 +10,29 @@ def classify0(inX,dataSet,labels,k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX,(dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
-    sqDistances = sqDiffmat.sum(axis=1)
+    sqDistances = sqDiffMat.sum(axis=1)
     distance = sqDistances**0.5
-    sprtedDistIndicies = distances.argsort()
+    sprtedDistIndicies = distance.argsort()
     classCount = {}
     for i in range(k):
-        voteIlabel = label[sortedDistindicies[i]]
+        voteIlabel = labels[sprtedDistIndicies[i]]
         classCount[voteIlabel] =classCount.get(voteIlabel,0) +1
-    sortedClassCount = sorted(classCount.iteritems(),key=operate.itemgetter(1),reverse=True)
+    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]    
-    
+
+def file2matrix(filename):
+    fr = open(filename)
+    arryOLines = fr.readlines()
+    numberOfLines = len(arryOLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    for line in arryOLines:
+        line = line.strip()
+        listFormLine = line.split('\t')
+        returnMat[index,:] = listFormLine[0:3]
+        classLabelVector.append(int(listFormLine[-1]))
+        index +=1
+    return returnMat,classLabelVector
     
 group,labels = creatDataSet()
