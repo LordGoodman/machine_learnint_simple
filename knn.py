3fddc77dcaaf4f4b27ae7b85e80dc06dae43f0c6
LordGoodman <201negative@sina.com>
2015-11-14 15:31:48 +0800

    Finished~

--- knn.py
+++ knn.py
@@ -1,10 +1,7 @@
+# -*- coding: utf-8 -*-
 from numpy import *
 import operator
 
-def creatDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    labels = ['A','A','B','B']
-    return group,labels
 
 def classify0(inX,dataSet,labels,k):
     dataSetSize = dataSet.shape[0]
@@ -35,4 +32,37 @@ def file2matrix(filename):
         index +=1
     return returnMat,classLabelVector
     
-group,labels = creatDataSet()
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals,(m,1))
+    normDataSet = normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+    
+def datingCalssTest(filePath):
+    hoRatio =0.1
+    datingDataMat,datingLabels = file2matrix(filePath)
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print "the classifier came back with: %d,the real answer is :%d"%(classifierResult,datingLabels[i])
+        if(classifierResult != datingLabels[i]):errorCount +=1.0
+    print "the total error rate is :%f"%(errorCount/float(numTestVecs))
+
+def classifyPerson(path):
+    resultList = ['not at all','in small does','in large does']
+    percentTats = float(raw_input("percentage of time spent playing vedio game?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+    datingDataMat,datingLabels = file2matrix(path)
+    normMat,ranges,minVal = autoNorm(datingDataMat)
+    inArr = array([ffMiles,percentTats,iceCream])
+    classifierResult = classify0((inArr-minVal)/ranges,normMat,datingLabels,3) #此处返回1 or 2 or 3
+    print "you will probably like this person:",resultList[classifierResult - 1]
+
