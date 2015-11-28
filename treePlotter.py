f926e631f32390ae1ff4bad244c1500d1fdeebe6
LordGoodman <201negative@sina.com>
2015-11-28 22:01:00 +0800

    add getnumleaf and get treedepth

--- treePlotter.py
+++ treePlotter.py
@@ -14,6 +14,29 @@ def createPlot():
     fig = plt.figure(1,facecolor='white')
     fig.clf()
     createPlot.ax1 = plt.subplot(111,frameon=False)
-    plotNode(U'决策节点',(0.5,0.1),(0.1,0.5),decisionNode)
-    plotNode(U'叶节点',(0.8,0.1),(0.3,0.8),leafNode)
+    plotNode('Decision node ',(0.5,0.1),(0.1,0.5),decisionNode)
+    plotNode('leaf node',(0.8,0.1),(0.3,0.8),leafNode)
     plt.show()
+    
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':numLeafs += getNumLeafs(secondDict[key])
+        else: numLeafs += 1
+    return numLeafs
+    
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict:
+        if type(secondDict[key]).__name__=="dict":thisDepth =1+ getTreeDepth(secondDict[key])
+        else: thisDepth = 1
+    if thisDepth > maxDepth :maxDepth = thisDepth
+    return maxDepth
+        
+    
+    
+    
