6fc5ac1dc29da85b50da11a8744d1bcf9d1453f4
LordGoodman <201negative@sina.com>
2016-03-08 21:43:06 +0800

    E-mail classification

--- Bayes.py
+++ Bayes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 def loadDataSet():
     postingList = [['my','dog','has','flea','problems','help','please'],
@@ -27,7 +27,15 @@ def setOfWords2Vec(vocabList,inputSet):
             returnVec[vocabList.index(word)] = 1
         else :print ("the word : %s is not in the Vocabulary!")%word
     return returnVec
-        
+
+def bagOfWords2Vec(vocabList,inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+     #   else :print ("the word : %s is not in the Vocabulary!")%word
+    return returnVec
+
 def trainNB0(trainMartix,trainCategory):
     numTrainDocs = len(trainMartix)
     numwords = len(trainMartix[0])
@@ -54,9 +62,9 @@ def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
      p0 = sum(vec2Classify*p0Vec) + np.log(1.0-pClass1)
      p1 = sum(vec2Classify*p1Vec) + np.log(pClass1)
      if p1>p0:
-         return 'abusive'
+         return 1
      else:
-         return 'cute'
+         return 0
          
 def testNB():
     listOfPosts,classVec = loadDataSet()
@@ -76,5 +84,63 @@ def testNB():
     #print thisDoc
     print testEntry,'classify as :',classifyNB(thisDoc,p0vec,p1vec,pAbusive)
 
-    
+#emailText = open('E:/MyCraft/machinelearninginaction/Ch04/email/ham/6.txt').read()
+#listOfTokens = regEx.split(emailText)  for testing
+
+def textParse(bigString):
+    import re
+    regEx = re.compile('\\W*')
+    listOfTokens = regEx.split(bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok)>2]
+
+
+def spamtext():
+    docList = []; classList = []; fullText = []
+    for i in range(1,26):
+        wordList = textParse(open("E:/MyCraft/machinelearninginaction/Ch04/email/spam/%d.txt"%i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
         
+        wordList = textParse(open("E:/MyCraft/machinelearninginaction/Ch04/email/ham/%d.txt"%i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+        
+        vocabList = creatVocabList(docList)
+        
+        trainingSet = range(50);testSet = []
+       
+    for i in range(10):
+            randIndex = int(np.random.uniform(0,len(trainingSet)))
+            testSet.append(trainingSet[randIndex])
+            del(trainingSet[randIndex])
+    trainMat = [];trainclass = []
+        
+    for doctIndex in trainingSet:
+            trainMat.append(setOfWords2Vec(vocabList,docList[doctIndex]))
+            trainclass.append(classList[doctIndex])
+
+    p0V,p1V,pSpam = trainNB0(np.array(trainMat),np.array(classList))
+    errorCount = 0
+    for testIndex in testSet:
+        test = setOfWords2Vec(vocabList,docList[testIndex])
+        p=classifyNB(np.array(test),p0V,p1V,pSpam)
+        if p !=classList[testIndex]:
+                errorCount += 1
+                print docList[testIndex],"has been classificated wrongly"
+    print "error rate is :",float(errorCount)/len(testSet)
+        
+        
+            
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
